c9f27c9475b415fa7e6516b128b9fd36d1abe788
itsabyssinia <abyssiniaqiw@gmail.com>
2024-08-11 12:59:55 -0700

    Add files via upload

--- 0x1.Introduction_ to_ Django/LibraryProject/manage.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/manage.py
@@ -20,3 +20,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bookshelf',  # Add this line
+]
+
